a716ac62a9f8599fbbbf3f536b16147e43232290
Adnan Almarzooq <cj-21-amm@hotmail.com>
2021-06-10 23:34:43 +0530

    adds unit test changes

--- Sprints/TeamProject/ITMatching.Tests/DeletePersonalDataPageTests.cs
+++ Sprints/TeamProject/ITMatching.Tests/DeletePersonalDataPageTests.cs
@@ -23,27 +23,44 @@ namespace ITMatching.Tests
     public class DeletePersonalDataPageTests
     {
         private DeletePersonalDataModel _pageModel;
-        private Mock<ITMatchingAppContext> _mockITMatchingAppContext;
+        private Mock<IItmuserRepository> _mockItmuserRepo;
+        private Mock<IExpertRepository> _mockExpertRepo;
+
+        private List<Itmuser> itmUsers;
+        private List<Expert> experts;
+
+        private const string ASPNET_USER_ID = "2a44c5fa-13a4-4477-82db-512da5e6f32a";
+        private const int ITMUSER_ID = 1;
+        private const int EXPERT_ID = 2;
 
         [SetUp]
         public void Setup()
         {
             var mockLogger = new Mock<ILogger<DeletePersonalDataModel>>();
-
-            // Mock the ITMatchingAppContext 
-            var itmUsers = new List<Itmuser>
+            itmUsers = new List<Itmuser>
             {
-                new Itmuser { Id = 1, FirstName = "Test", LastName = "BBB", PhoneNumber = "9998524256", Email = "jame@im.com" , UserName = "jame@im.com", AspNetUserId = "2a44c5fa-13a4-4477-82db-512da5e6f32a" },
+                new Itmuser { Id = ITMUSER_ID, FirstName = "Test", LastName = "BBB", PhoneNumber = "9998524256", Email = "jame@im.com" , UserName = "jame@im.com", AspNetUserId = ASPNET_USER_ID },
                 new Itmuser { Id = 2, FirstName = "Test", LastName = "ZZZ", PhoneNumber = "9985242568", Email = "zzz@im.com" , UserName = "zzz@im.com", AspNetUserId = Guid.NewGuid().ToString()  },
                 new Itmuser { Id = 3, FirstName = "Test", LastName = "AAA", PhoneNumber = "9985242526", Email = "aaa@im.com" , UserName = "aaa@im.com", AspNetUserId = Guid.NewGuid().ToString() },
-            }.AsQueryable();
+            };
 
-            var mockItmuserSet = itmUsers.AsQueryable().BuildMockDbSet();
+            experts = new List<Expert>
+            {
+                new Expert { Id = EXPERT_ID, ItmuserId = ITMUSER_ID, WorkSchedule = "Work Schedule", IsAvailable = false },
+                new Expert { Id = 45, ItmuserId = 22, WorkSchedule = "Work Schedule", IsAvailable = true },
+                new Expert { Id = 55, ItmuserId = 33, WorkSchedule = "Work Schedule", IsAvailable = false },
+            };
 
-            _mockITMatchingAppContext = new Mock<ITMatchingAppContext>();
-            _mockITMatchingAppContext.Setup(m => m.Itmusers).Returns(mockItmuserSet.Object);
+            // Mock the Itmuser repository
+            _mockItmuserRepo = new Mock<IItmuserRepository>();
+            _mockItmuserRepo.Setup(m => m.GetByAspNetUserIdAsync(ASPNET_USER_ID)).ReturnsAsync(itmUsers.Where(u => u.AspNetUserId == ASPNET_USER_ID).FirstOrDefault()).Verifiable();
 
-            _pageModel = new DeletePersonalDataModel(new FakeUserManager(), new FakeSignInManager(), mockLogger.Object, _mockITMatchingAppContext.Object);
+            // Mock the Expert repository
+            _mockExpertRepo = new Mock<IExpertRepository>();
+            _mockExpertRepo.Setup(m => m.GetByItmUserIdAsync(ITMUSER_ID)).ReturnsAsync(experts.Where(e => e.ItmuserId == ITMUSER_ID).FirstOrDefault()).Verifiable();
+            _mockExpertRepo.Setup(m => m.DeleteServicesAsync(EXPERT_ID)).Verifiable();
+
+            _pageModel = new DeletePersonalDataModel(new FakeUserManager(), new FakeSignInManager(), mockLogger.Object, _mockItmuserRepo.Object, _mockExpertRepo.Object);
         }
 
         [Test]
@@ -59,11 +76,9 @@ namespace ITMatching.Tests
             IActionResult result = await _pageModel.OnPostAsync();
 
             //Assert
-            var itmUser = await _mockITMatchingAppContext.Object.Itmusers.Where(u => u.UserName == userName).FirstOrDefaultAsync();
-            var updatedItmUser = await _mockITMatchingAppContext.Object.Itmusers.Where(u => u.UserName == updatedUserName).FirstOrDefaultAsync();
-
-            Assert.That(itmUser, Is.Null);
-            Assert.That(updatedItmUser, Is.Not.Null);
+            _mockItmuserRepo.Verify(mock => mock.GetByAspNetUserIdAsync(ASPNET_USER_ID), Times.Once());
+            _mockExpertRepo.Verify(mock => mock.GetByItmUserIdAsync(ITMUSER_ID), Times.Once());
+            _mockExpertRepo.Verify(mock => mock.DeleteServicesAsync(EXPERT_ID), Times.Once());
             Assert.That((result as PageResult), Is.Null);
             Assert.That((result as RedirectResult), Is.Not.Null);
             Assert.That((result as RedirectResult).Url, Is.EqualTo("~/"));
@@ -82,11 +97,9 @@ namespace ITMatching.Tests
             IActionResult result = await _pageModel.OnPostAsync();
 
             //Assert
-            var itmUser = await _mockITMatchingAppContext.Object.Itmusers.Where(u => u.UserName == userName).FirstOrDefaultAsync();
-            var updatedItmUser = await _mockITMatchingAppContext.Object.Itmusers.Where(u => u.UserName == updatedUserName).FirstOrDefaultAsync();
-
-            Assert.That(itmUser, Is.Not.Null);
-            Assert.That(updatedItmUser, Is.Null);
+            _mockItmuserRepo.Verify(mock => mock.GetByAspNetUserIdAsync(ASPNET_USER_ID), Times.Never());
+            _mockExpertRepo.Verify(mock => mock.GetByItmUserIdAsync(ITMUSER_ID), Times.Never());
+            _mockExpertRepo.Verify(mock => mock.DeleteServicesAsync(EXPERT_ID), Times.Never());
             Assert.That(_pageModel.ModelState.IsValid, Is.False);
             Assert.That(_pageModel.ModelState.Count, Is.EqualTo(1));
             Assert.That(_pageModel.ModelState.ErrorCount, Is.EqualTo(1));
